da86a4799d53a42d89dbb40b49cd04ef9a51b0b3
Alayn Gortazar <alayn@barnetik.com>
2021-10-28 00:17:55 +0200

    Added Emisor entity

--- src/Barnetik/Tbai/InvoiceSubject.php
+++ src/Barnetik/Tbai/InvoiceSubject.php
@@ -4,13 +4,19 @@ namespace Barnetik\Tbai;
 
 class InvoiceSubject
 {
+    const EMITED_BY_EMISOR = 'N';
+    const EMITED_BY_THIRD_PARTY = 'T';
+    const EMITED_BY_RECEPTOR = 'D';
+
     protected $emisor;
     protected $receptors = [];
+    protected $emitedBy;
 
-    public function __construct($emisor, Receptor $receptor)
+    public function __construct(Emisor $emisor, Receptor $receptor, $emitedBy)
     {
-        $this->emisor;
+        $this->emisor = $emisor;
         $this->addReceptor($receptor);
+        $this->emitedBy = $emitedBy;
     }
 
     public function addReceptor(Receptor $receptor)
@@ -26,5 +32,15 @@ class InvoiceSubject
     public function receptors()
     {
         return $this->receptors;
+    }
+
+    public function multipleReceptors()
+    {
+        return sizeof($this->receptors) > 1;
+    }
+
+    public function emitedBy()
+    {
+        return $this->emitedBy;
     }
 }
