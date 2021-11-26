a4399dc739940fa84f483a9ad3192ed9e7c184ea
Alayn Gortazar <alayn@barnetik.com>
2021-11-26 09:47:31 +0100

    Allow changing vendor name and version (needed for development environment)

--- src/Barnetik/Tbai/Fingerprint/Vendor.php
+++ src/Barnetik/Tbai/Fingerprint/Vendor.php
@@ -13,11 +13,15 @@ class Vendor implements TbaiXml
 
     private string $license;
     private string $developerId;
+    private string $name;
+    private string $version;
 
-    public function __construct(string $license, string $developerId)
+    public function __construct(string $license, string $developerId, string $name = self::NAME, string $version = self::VERSION)
     {
         $this->license = $license;
         $this->developerId = $developerId;
+        $this->name = $name;
+        $this->version = $version;
     }
 
     public function xml(DOMDocument $domDocument): DOMNode
@@ -30,8 +34,8 @@ class Vendor implements TbaiXml
         $vendor->append(
             $domDocument->createElement('LicenciaTBAI', $this->license),
             $developer,
-            $domDocument->createElement('Nombre', self::NAME),
-            $domDocument->createElement('Version', self::VERSION)
+            $domDocument->createElement('Nombre', $this->name),
+            $domDocument->createElement('Version', $this->version)
         );
         return $vendor;
     }
