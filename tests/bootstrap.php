3a7fc03d071caa1c57c5efdbf84bf0a4c685ab06
Alayn Gortazar <alayn@barnetik.com>
2022-03-28 14:26:54 +0200

    Set default stream context on phpunit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,4 +4,13 @@ if (!is_file($autoloadFile = __DIR__ . '/../vendor/autoload.php')) {
     throw new \LogicException('Could not find autoload.php in vendor/. Did you run "composer install --dev"?');
 }
 
+stream_context_set_default([
+    'http' => [
+        'follow_location' => 1,
+        'protocol_version' => '1.1',
+        'max_redirects' => 20,
+        'header' => []
+    ]
+]);
+
 require $autoloadFile;
