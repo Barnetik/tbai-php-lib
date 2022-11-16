7df18fa0dc1efee7c5e0bf8005e2155e1f7c7ed4
Alayn Gortazar <alayn@barnetik.com>
2022-11-16 13:38:12 +0100

    Added VatId tests for NIF's

---
+++ tests/Barnetik/Tbai/ValueObject/VatIdTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Test\Barnetik\Tbai\ValueObject;
+
+use Barnetik\Tbai\Exception\InvalidVatIdException;
+use Barnetik\Tbai\ValueObject\VatId;
+use Exception;
+use PHPUnit\Framework\TestCase;
+
+class VatIdTest extends TestCase
+{
+    public function test_ifz_with_wrong_format_throw_exception(): void
+    {
+        try {
+            new VatId("1234567-S", VatId::VAT_ID_TYPE_IFZ);
+            $this->fail();
+        } catch (InvalidVatIdException $e) {
+        }
+
+        try {
+            new VatId("0134567S", VatId::VAT_ID_TYPE_IFZ);
+            $this->fail();
+        } catch (InvalidVatIdException $e) {
+        }
+
+        $this->assertTrue(true);
+    }
+
+    public function test_ifz_with_leading_zeros_do_not_throw_exception(): void
+    {
+        try {
+            new VatId("01234567S", VatId::VAT_ID_TYPE_IFZ);
+            new VatId("00000567S", VatId::VAT_ID_TYPE_IFZ);
+            $this->assertTrue(true);
+        } catch (InvalidVatIdException $e) {
+            $this->fail();
+        }
+    }
+}
