102dc76ba9ab5f168b66b1bfa9c4ffca772dcbf1
Alayn Gortazar <alayn@barnetik.com>
2025-05-19 10:58:35 +0200

    fix #63: Bizkaia and Araba Policy Identifier and Digest where outdated

--- src/Barnetik/Tbai/Xades/Araba.php
+++ src/Barnetik/Tbai/Xades/Araba.php
@@ -7,6 +7,6 @@ use lyquidity\xmldsig\XMLSecurityDSig;
 class Araba extends TicketBai
 {
     const POLICY_IDENTIFIER = 'https://ticketbai.araba.eus/tbai/sinadura/';
-    const POLICY_DIGEST = 'd69VEBc4ED4QbwnDtCA2JESgJiw+rwzfutcaSl5gYvM=';
+    const POLICY_DIGEST = '4Vk3uExj7tGn9DyUCPDsV9HRmK6KZfYdRiW3StOjcQA=';
     const ALGORITHM = XMLSecurityDSig::SHA256;
 }
--- src/Barnetik/Tbai/Xades/Bizkaia.php
+++ src/Barnetik/Tbai/Xades/Bizkaia.php
@@ -6,7 +6,7 @@ use lyquidity\xmldsig\XMLSecurityDSig;
 
 class Bizkaia extends TicketBai
 {
-    const POLICY_IDENTIFIER = 'https://www.batuz.eus/fitxategiak/batuz/ticketbai/sinadura_elektronikoaren_zehaztapenak_especificaciones_de_la_firma_electronica_v1_0.pdf';
-    const POLICY_DIGEST = 'Quzn98x3PMbSHwbUzaj5f5KOpiH0u8bvmwbbbNkO9Es=';
+    const POLICY_IDENTIFIER = 'https://www.batuz.eus/fitxategiak/batuz/ticketbai/sinadura_elektronikoaren_zehaztapenak_especificaciones_de_la_firma_electronica_v1_1.pdf';
+    const POLICY_DIGEST = 'K2baIY0fk8jbkPHkffk5F5C46O5VuzDwH21dAovjVRs=';
     const ALGORITHM = XMLSecurityDSig::SHA256;
 }
